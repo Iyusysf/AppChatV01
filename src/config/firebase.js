b7b78ba1ece7c39a866f24e63cb24c9b85f8e1f6
Iyusysf <42061520+Iyusysf@users.noreply.github.com>
2020-07-17 18:07:36 +0700

    Initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBpn6nz1oxagMfdNNkRE0L2ihh9zTVH8_8",
+  authDomain: "supervoucher-141bc.firebaseapp.com",
+  databaseURL: "https://supervoucher-141bc.firebaseio.com",
+  projectId: "supervoucher-141bc",
+  storageBucket: "supervoucher-141bc.appspot.com",
+  messagingSenderId: "456863107610",
+  appId: "1:456863107610:web:91189032e4e567853a9fa3"
+};
+
+export default firebaseConfig;
