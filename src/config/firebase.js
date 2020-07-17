a3ff63fc03f1bea94bb157fe6f2e6b63ed7bfc7c
Iyusysf <42061520+Iyusysf@users.noreply.github.com>
2020-07-17 20:17:01 +0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBpn6nz1oxagMfdNNkRE0L2ihh9zTVH8_8",
-  authDomain: "supervoucher-141bc.firebaseapp.com",
-  databaseURL: "https://supervoucher-141bc.firebaseio.com",
-  projectId: "supervoucher-141bc",
-  storageBucket: "supervoucher-141bc.appspot.com",
-  messagingSenderId: "456863107610",
-  appId: "1:456863107610:web:91189032e4e567853a9fa3"
+  apiKey: "AIzaSyAd2t_-KYygVf5iJUQNc44YXPtWu6PdLXU",
+  authDomain: "appchatv01.firebaseapp.com",
+  databaseURL: "https://appchatv01.firebaseio.com",
+  projectId: "appchatv01",
+  storageBucket: "appchatv01.appspot.com",
+  messagingSenderId: "1034171900730",
+  appId: "1:1034171900730:web:53dbf8f78b72c06dd42603"
 };
 
 export default firebaseConfig;
